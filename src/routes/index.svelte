c1ebe58e9cc56f3a39869c4bef2515a27d9ac4ec
trick0 <0waltercossu@gmail.com>
2022-06-18 20:44:07 +0200

    first commity

---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script>
+	const test = [
+		{
+			label: 'Page 1',
+			code: 'page1'
+		},
+		{
+			label: 'Page 2',
+			code: 'page2'
+		}
+	];
+</script>
+
+<main class="w-full h-full">
+    {#each test as page}
+        <div class="page h-full" id="{page.code}">
+            { page.label }
+        </div>
+    {/each}
+</main>
+
+<style>
+	main {
+		scroll-snap-type: y mandatory;
+		overflow-y: scroll;
+	}
+
+	.page {
+		scroll-snap-align: start;
+	}
+</style>
+
