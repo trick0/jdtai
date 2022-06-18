7e239a91b2ee15a1259ce774ae44ff2c9b309d8f
trick0 <0waltercossu@gmail.com>
2022-06-18 21:16:08 +0200

    Updates

--- manifest.js
+++ manifest.js
@@ -3,7 +3,7 @@ export const manifest = {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		entry: {"file":"start-98abfd38.js","js":["start-98abfd38.js","chunks/index-92fe9422.js"],"css":[]},
+		entry: {"file":"start-1108987d.js","js":["start-1108987d.js","chunks/index-92fe9422.js"],"css":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
