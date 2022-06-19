2c498dee527ddb044bc7e07ef03c47460de25330
trick0 <0waltercossu@gmail.com>
2022-06-19 18:43:12 +0200

    added pages and fixes

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		paths: { base: '/jdtai' },
+		paths: { base: '/' },
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
