e19db4d7051ee39c9056a556565ddfb2a5a44cde
trick0 <0waltercossu@gmail.com>
2022-06-19 19:35:38 +0200

    path 01

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: { base: "/jdtai", },
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
