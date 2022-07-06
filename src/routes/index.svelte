c38584e8dd0112991550a15d8ace47e4bdaded53
w.cossu <walter.cossu@realt.it>
2022-07-06 12:34:24 +0200

    content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,9 +7,16 @@
 	{#each pages as page}
 		<div 
 			class="page h-full p-8 flex flex-col" 
-			id={page.title} 
+			id={page.title + page.code} 
 			style="justify-content: {page.position}; background-size: {page.backgroundSize}; background-image: url('{page.code}.png'); background-repeat: no-repeat; background-position: {page.backgroundPosition}; background-color: {page.background}; color: {page.color}">
-			<SvelteMarkdown source={page.content} />
+			<div class="text-2xl font-bold my-4">
+				{page.title}
+			</div>
+			{#each page.content as row}
+				<div class="my-2 w-1/2">
+					<SvelteMarkdown source={ row } />
+				</div>
+			{/each}
 		</div>
 	{/each}
 </main>
@@ -22,5 +29,14 @@
 
 	.page {
 		scroll-snap-align: start;
+	}
+
+	:global(.tag) {
+		display: inline;
+		margin-right: 5px;
+		color: white;
+		background: black;
+		border-radius: 3px;
+		padding: 6px;
 	}
 </style>
